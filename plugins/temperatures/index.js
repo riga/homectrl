813202b15a4f48ce829b1057557f21430525f5f3
marcel <rieger@physik.rwth-aachen.de>
2014-10-23 16:46:20 +0200

    Major refactoring, add plugin mechanism.

---
+++ plugins/temperatures/index.js
@@ -0,0 +1,16 @@
+// temperatures/index.js
+
+var hc = require("homectrl");
+
+
+module.exports = hc.HCPlugin._extend({
+
+  setup: function() {
+    console.log("setup called of temperatures plugin");
+
+    this.GET("/", function(req, res) {
+      // hc.send(res);
+      res.render("index.jade");
+    });
+  }
+});
