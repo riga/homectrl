8af7bb037b85a96d0cc2a4ac3368ae953c27b075
marcel <rieger@physik.rwth-aachen.de>
2014-10-27 19:19:43 +0100

    Gpio plugin UI draft.

--- plugins/gpio/index.js
+++ plugins/gpio/index.js
@@ -1,5 +1,5 @@
 var hc    = require("homectrl"),
-    gpio  = require("gpio"),
+    GPIO  = require("gpio"),
     async = require("async");
 
 
@@ -8,39 +8,51 @@ module.exports = hc.Plugin._extend({
   setup: function() {
     this._super();
 
-    this.pins = {};
+    this.gpios = {};
 
     // routes
     this.setupRoutes();
 
     // setup pins
-    this.config.get("pins").forEach(this.setupPin.bind(this));
+    // this.config.get("gpioNums").forEach(this.setupGpio.bind(this));
+    this.config.get("gpioNums").forEach(function(num) { this.gpios[num] = {}; }, this);
 
-    this.logger.info("Setup gpio with pins", this.config.get("pins"));
+    this.logger.info("gpio: setup with gpio numbers", Object.keys(this.gpios).join(", "));
   },
 
   setupRoutes: function() {
     var self = this;
 
+    this.GET("/gpios", function(req, res) {
+      var data = Object.keys(self.gpios).map(function(num) {
+        var gpio = self.gpios[num];
+        return {
+          num      : num,
+          direction: gpio.direction
+        };
+      });
+      hc.send(res, data);
+    });
+
     this.POST("/set", function(req, res) {
-      var n = parseInt(req.param("pin"));
-      if (isNaN(n)) return hc.send(res, 400, "pin is not an integer");
+      var num = parseInt(req.param("gpio"));
+      if (isNaN(num)) return hc.send(res, 400, "gpio is not an integer");
 
       var value = parseInt(req.param("value"));
       if (isNaN(value)) return hc.send(res, 400, "value is not an integer");
 
-      self.setValue(n, value);
+      self.setValue(num, value);
       hc.send(res);
     });
 
     this.POST("/direction", function(req, res) {
-      var n = parseInt(req.param("pin"));
-      if (isNaN(n)) return hc.send(res, 400, "pin is not an integer");
+      var num = parseInt(req.param("gpio"));
+      if (isNaN(num)) return hc.send(res, 400, "gpio is not an integer");
 
       var direction = req.param("direction");
       if (!direction) return hc.send(res, 400, "direction not set");
 
-      self.setDirection(n, direction);
+      self.setDirection(num, direction);
       hc.send(res);
     });
 
@@ -52,22 +64,25 @@ module.exports = hc.Plugin._extend({
     return this;
   },
 
-  setupPin: function(n, callback) {
+  setupGpio: function(num, callback) {
     var self = this;
 
     callback = callback || function(){};
 
-    if (this.pins[n]) return callback("pin %s already setup", n);
-    var pin = this.pins = gpio.export(n, {
+    if (this.gpios[num]) return callback("gpio %s already setup", num);
+
+    var gpio = this.gpios[num] = GPIO.export(num, {
       direction: "out",
       interval : 100,
       ready    : function() {
-        pin.on("valueChange", function(value) {
-          self.sendMessage("all", "valueChange", n, value);
+        gpio.set(0);
+
+        gpio.on("valueChange", function(value) {
+          self.sendMessage("all", "valueChange", num, value);
         });
 
-        pin.on("directionChange", function(direction) {
-          self.sendMessage("all", "directionChange", n, direction);
+        gpio.on("directionChange", function(direction) {
+          self.sendMessage("all", "directionChange", num, direction);
         });
 
         callback(null);
@@ -76,21 +91,21 @@ module.exports = hc.Plugin._extend({
     return this;
   },
 
-  setDirection: function(n, direction) {
-    var pin = self.pins[n];
-    if (pin) pin.setDirection(direction);
+  setDirection: function(num, direction) {
+    var gpio = this.gpios[n];
+    if (gpio) gpio.setDirection(direction);
     return this;
   },
 
-  setValue: function(n, value) {
-    var pin = self.pins[n];
-    if (pin) pin.set(value);
+  setValue: function(num, value) {
+    var gpio = this.gpios[num];
+    if (gpio) gpio.set(value);
     return this;
   },
 
   resetValues: function() {
-    Objects.keys(this.pins).forEach(function(n) {
-      this.setValue(n, 0);
+    Object.keys(this.gpios).forEach(function(num) {
+      this.setValue(num, 0);
     }, this);
     return this;
   }
--- plugins/gpio/static/js/index.js
+++ plugins/gpio/static/js/index.js
@@ -5,16 +5,89 @@ define(["homectrl"], function(hc) {
     setup: function() {
       this._super();
 
-      var self = this;
+      this.dirTogglesVisible = true;
 
       this.addCss("styles.min.css");
       this.setLabel("GPIO");
       this.setIcon("heart");
 
-      this.getTemplate("index.jade", function(tmpl) {
-        self.nodes.$content.html(tmpl)
-          .find("input").bootstrapSwitch();
+      this.dirSwitchConfig = {
+        onText  : "In",
+        offText : "Out",
+        onColor : "info",
+        offColor: "info",
+        size    : "small"
+      };
+      this.valueSwitchConfig = {
+        onText  : "1",
+        offText : "0",
+        onColor : "success",
+        offColor: "danger",
+        readonly: false
+      };
+
+      this.setupUI();
+    },
+
+    setupUI: function() {
+      var self = this;
+
+      this.GET("/gpios", function(res) {
+        self.getTemplate("index.jade", function(tmpl) {
+
+          var directives = {
+            gpio: {
+              action: function(data) {
+                $(data.element).attr("id", this.num);
+              }
+            }
+          };
+
+          // render
+          self.nodes.$content.html(tmpl).find("#gpios").render(res.data, directives);
+          self.nodes.$content.find(".direction > input").bootstrapSwitch(self.dirSwitchConfig);
+          self.nodes.$content.find(".value > input").bootstrapSwitch(self.valueSwitchConfig);
+
+          // store nodes
+          self.nodes.$dirToggle = self.nodes.$content.find("#dir-toggle");
+          self.nodes.$resetAll  = self.nodes.$content.find("#reset-all");
+
+          // events
+          self.nodes.$dirToggle.click(function(event) {
+            self.toggleDirectionToggles();
+          });
+
+          self.nodes.$resetAll.click(function() {
+            self.POST("/resetall");
+          });
+
+          // apply data from cookies
+          var dirTogglesVisible = $.cookie("dirTogglesVisible") != "false";
+          self.toggleDirectionToggles(dirTogglesVisible);
+        });
       });
+    },
+
+    toggleDirectionToggles: function(desired) {
+      if (typeof desired == "boolean" && desired == this.dirTogglesVisible) {
+        // no change => nothing happens
+        return this;
+      }
+
+      this.dirTogglesVisible = !this.dirTogglesVisible;
+      $.cookie("dirTogglesVisible", this.dirTogglesVisible);
+
+      if (this.dirTogglesVisible) {
+        this.nodes.$content.find(".direction").slideDown(200);
+        this.nodes.$dirToggle.find("span").html(" Hide Directions");
+        this.nodes.$dirToggle.find("i").attr("class", "glyphicon glyphicon-eye-close");
+      } else {
+        this.nodes.$content.find(".direction").slideUp(200);
+        this.nodes.$dirToggle.find("span").html(" Show Directions");
+        this.nodes.$dirToggle.find("i").attr("class", "glyphicon glyphicon-eye-open");
+      }
+
+      return this;
     }
 
   });
