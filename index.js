c4e77e479c6b7b7110d46a91d226236906bf1183
marcel <rieger@physik.rwth-aachen.de>
2015-04-15 11:46:00 +0200

    Make symlinks relative in startup script.

--- index.js
+++ index.js
@@ -50,18 +50,18 @@ fs.readdirSync(samplesPath).forEach(function(file) {
 
 
 /**
- * Create symlinks to simplify imports.
+ * Create relative symlinks to simplify imports.
  */
 
 // create a "fake" node module to be able to require "homectrl" in plugins
 // node_modules/homectrl/index.js -> lib/public.js
-util.createSymlink("lib/public.js", "node_modules/homectrl/index.js");
+util.createSymlink("../../lib/public.js", "node_modules/homectrl/index.js");
 
 // make certain files available through the static mount point
-util.createSymlink("node_modules/jclass/index.js", "static/vendor/jclass.js");
-util.createSymlink("node_modules/eventemitter2/lib/eventemitter2.js",
+util.createSymlink("../../node_modules/jclass/index.js", "static/vendor/jclass.js");
+util.createSymlink("../../node_modules/eventemitter2/lib/eventemitter2.js",
                    "static/vendor/eventemitter2.js");
-util.createSymlink("node_modules/async/lib/async.js", "static/vendor/async.js");
+util.createSymlink("../../node_modules/async/lib/async.js", "static/vendor/async.js");
 
 
 /**
