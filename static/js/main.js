adee3b50079ca26a2e16f08dff7ae3d57c24c36a
marcel <rieger@physik.rwth-aachen.de>
2015-05-07 14:25:11 +0200

    Add highcharts.

--- static/js/main.js
+++ static/js/main.js
@@ -20,15 +20,17 @@ var paths = {
   bootstrap     : "vendor/bootstrap-3.2.0/js/bootstrap.min",
   bsSwitch      : "vendor/bootstrap.switch-3.3.2/js/bootstrap.switch.min",
   io            : "vendor/socket.io-1.3.5.min",
+  highcharts    : "vendor/highcharts-4.1.5.min",
   async         : "vendor/async"
 };
 
 // change some paths in case CDN's are used
 if (window.hcData.useCdn) {
-  paths.jquery    = "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min";
-  paths.jqCookie  = "//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min";
-  paths.bootstrap = "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min";
-  paths.io        = "//cdn.socket.io/socket.io-1.3.5";
+  paths.jquery     = "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min";
+  paths.jqCookie   = "//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min";
+  paths.bootstrap  = "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min";
+  paths.io         = "//cdn.socket.io/socket.io-1.3.5";
+  paths.highcharts = "//cdnjs.cloudflare.com/ajax/libs/highcharts/4.1.5/highcharts.js";
 }
 
 
@@ -45,6 +47,7 @@ require.config({
     jqLogger      : [ "jquery" ],
     bootstrap     : [ "jquery" ],
     bsSwitch      : [ "bootstrap" ],
+    highcharts    : [ "jquery" ],
     homectrl      : [ "jqCookie", "jqTransparency", "bsSwitch", "jqLogger" ]
   },
   map: {
